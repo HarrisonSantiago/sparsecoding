67a4c195184b46a8147f4055381ea1e96a92ef84
HarrisonSantiago <harrisonsantiago4@gmail.com>
2024-11-11 20:01:32 -0500

    tests

--- tests/transforms/test_whiten.py
+++ tests/transforms/test_whiten.py
@@ -1,26 +1,28 @@
 import torch
 import unittest
 
-from transforms import whiten
+from sparsecoding.transforms import whiten
 
 class TestWhitener(unittest.TestCase):
     def test_pca(self):
-        N = 100
-        D = 10
+        N = 400
+        C = 1
+        H = 16
+        W = 16
 
         torch.manual_seed(42)
 
-        data = torch.rand((N, D), dtype=torch.float32)
+        data = torch.rand((N, C, H, W), dtype=torch.float32)
         whitened_data = whiten(data, algorithm = 'pca')
 
         assert torch.allclose(
             torch.mean(whitened_data, dim=0),
-            torch.zeros(D, dtype=torch.float32),
+            torch.zeros(1, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have zero mean."
         assert torch.allclose(
-            torch.cov(whitened_data.T),
-            torch.eye(D, dtype=torch.float32),
+            torch.cov(whitened_data.reshape(N, -1).T),
+            torch.eye(C * H * W, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have unit (identity) covariance."
 
@@ -33,22 +35,24 @@ class TestWhitener(unittest.TestCase):
 
 
     def test_zca(self):
-        N = 100
-        D = 10
+        N = 400
+        C = 1
+        H = 16
+        W = 16
 
         torch.manual_seed(42)
 
-        data = torch.rand((N, D), dtype=torch.float32)
-        whitened_data = whiten(data, algorithm = 'zca')
+        data = torch.rand((N, C, H, W), dtype=torch.float32)
+        whitened_data = whiten(data, algorithm = 'zca', epsilon = 1e-2)
 
         assert torch.allclose(
             torch.mean(whitened_data, dim=0),
-            torch.zeros(D, dtype=torch.float32),
+            torch.zeros(1, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have zero mean."
         assert torch.allclose(
-            torch.cov(whitened_data.T),
-            torch.eye(D, dtype=torch.float32),
+            torch.cov(whitened_data.reshape(N, -1).T),
+            torch.eye(C * H * W, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have unit (identity) covariance."
 
@@ -60,22 +64,25 @@ class TestWhitener(unittest.TestCase):
         )), "If an eigenvalue is 0, should not get NaNs."
 
     def test_cholesky(self):
-        N = 100
-        D = 10
+        N = 400
+        C = 1
+        H = 16
+        W = 16
 
         torch.manual_seed(42)
 
-        data = torch.rand((N, D), dtype=torch.float32)
+        data = torch.rand((N, C, H, W), dtype=torch.float32)
         whitened_data = whiten(data, algorithm = 'cholesky')
-
+        print(torch.cov(whitened_data.reshape(N, -1)))
+        
         assert torch.allclose(
-            torch.mean(whitened_data, dim=0),
-            torch.zeros(D, dtype=torch.float32),
+            torch.mean(whitened_data),
+            torch.zeros(1, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have zero mean."
         assert torch.allclose(
-            torch.cov(whitened_data.T),
-            torch.eye(D, dtype=torch.float32),
+            torch.cov(whitened_data.reshape(N, -1).T),
+            torch.eye(C * H * W, dtype=torch.float32),
             atol=1e-3,
         ), "Whitened data should have unit (identity) covariance."
 
